250e6b0c142fb6b867d95502265436016beee2a9
Hridoy421eng <mdhihridoy1@gmailcom>
2025-04-10 17:00:44 +0600

    added the final code

--- js/btn-task.js
+++ js/btn-task.js
@@ -14,8 +14,25 @@ for (let i = 0; i <buttons.length; i++){
         document.getElementById('nav-task').innerText = convertedNavTask;
         this.disabled = true; 
 
-        
-
+        if(this.disabled=true){
+            
+            const container = document.getElementById('parent-div');
+            const card = this.closest('.card-body');
+            const cardHeading = card.querySelector('.card-head').innerText;
+            const p = document.createElement("p");
+            const currentTime = new Date().toLocaleTimeString();
+            
+            p.innerText = `
+            You have completed the task ${cardHeading} at ${currentTime} 
+            `
+            p.classList.add('mx-5', 'my-5', 'bg-[#F4F7FF]', 'rounded-xl', 'p-5');
+            container.appendChild(p);
+        }
+        document.getElementById('clear-history-btn').addEventListener('click', function(){
+            const container = document.getElementById('parent-div');
+            container.innerHTML = "";
+        })
+       
 
         if(convertedTaskAssigned === 0){
             alert('congrats!!! You have completed all the current task');
